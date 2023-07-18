3e1c74c819d882b9f3942bb43ddb8c467c674c15
ignam <ignacio.mendez@ing.austral.edu.ar>
2023-07-18 17:08:07 -0300

    Commit nacho

--- src/main/java/com/example/Snippet/Searcher/SnippetController.java
+++ src/main/java/com/example/Snippet/Searcher/SnippetController.java
@@ -2,6 +2,19 @@ package com.example.Snippet.Searcher;
 
 import java.util.List;
 
+import ast.AbstractSyntaxTree;
+import exceptions.*;
+import org.antlr.v4.runtime.Lexer;
+import org.austral.edu.Exceptions.AssignationException;
+import org.austral.edu.Exceptions.IncompatibilityException;
+import org.austral.edu.Exceptions.InterpretException;
+import org.austral.edu.Exceptions.NotDefinedException;
+import org.austral.edu.InterpreterV1;
+import org.austral.edu.LexerV1;
+import org.austral.edu.Results.ClassicResult;
+import org.austral.edu.Results.Result;
+import org.austral.edu.StringInput;
+import org.austral.edu.Token;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import parser.ParserV1;
 
 @RestController
 
@@ -28,6 +42,25 @@ class SnippetController {
         return repository.findAll();
     }
     // end::get-aggregate-root[]
+
+    @GetMapping("/run")
+    String run() throws UnclosedBracesException, UnclosedParenthesesException, UnclosedStringLiteralException, UnexpectedTokenException, ExpectedTokenException, IncompatibilityException, DividedByZeroException, AssignationException, InterpretException, IncompatibleOperationException, NotDefinedException, VariableDoesntExistsException {
+        LexerV1 lexer = new LexerV1();
+        ParserV1 parser = new ParserV1();
+        ClassicResult result = new ClassicResult();
+        InterpreterV1 interpreter = new InterpreterV1(result);
+
+        List<Token> tokens = lexer.lex(new StringInput("let x: number = 4;" +
+                "let y: number = 6;" +
+                "println(x + y);"));
+
+        AbstractSyntaxTree ast = parser.parse(tokens);
+
+        interpreter.interpret(ast);
+
+        return interpreter.getResult().toString();
+
+    }
 
     @PostMapping("/add")
     Snippet newSnippet(@RequestBody Snippet newSnippet) {
